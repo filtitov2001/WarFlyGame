4053268cf37fe0042f8b8dc6b61a4a0c3706100f
Felix Titov <titow.feliks@yandex.ru>
2022-06-18 15:36:31 +0300

    Created plane animation

--- WarFlyGame/GameScene.swift
+++ WarFlyGame/GameScene.swift
@@ -13,34 +13,15 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+
     var player: PlayerPlane!
     
     override func didMove(to view: SKView) {
-        configureStartScene()
         
+        configureStartScene()
         spawnClouds()
         spawnIslands()
         player.performFly()
-    }
-    
-    fileprivate func configureStartScene() {
-        let screenCentrePoint = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
-        let background = Background.populateBackground(at: screenCentrePoint)
-        background.size = self.size
-        
-        self.addChild(background)
-        
-        let screen = UIScreen.main.bounds
-        
-        let island1 = Island.populate(at: CGPoint(x: 100, y: 200))
-        self.addChild(island1)
-        
-        let island2 = Island.populate(at: CGPoint(x: self.size.width - 100, y: self.size.height - 200))
-        self.addChild(island2)
-        
-        player = PlayerPlane.populate(at: CGPoint(x: screen.size.width / 2, y: 100))
-        self.addChild(player)
-        
     }
     
     fileprivate func spawnClouds() {
@@ -67,15 +48,35 @@ class GameScene: SKScene {
         run(spawnIslandForever)
     }
     
+    fileprivate func configureStartScene() {
+        let screenCenterPoint = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        let background = Background.populateBackground(at: screenCenterPoint)
+        background.size = self.size
+        self.addChild(background)
+        
+        let screen = UIScreen.main.bounds
+        
+        let island1 = Island.populate(at: CGPoint(x: 100, y: 200))
+        self.addChild(island1)
+        
+        let island2 = Island.populate(at: CGPoint(x: self.size.width - 100, y: self.size.height - 200))
+        self.addChild(island2)
+        
+        player = PlayerPlane.populate(at: CGPoint(x: screen.size.width / 2, y: 100))
+        self.addChild(player)
+        
+
+    }
+    
     override func didSimulatePhysics() {
+        
         
         player.checkPosition()
         
-        enumerateChildNodes(withName: "backgroundSprite") { node, stop in
+        enumerateChildNodes(withName: "backgroundSprite") { (node, stop) in
             if node.position.y < -199 {
                 node.removeFromParent()
             }
         }
-        
     }
 }
