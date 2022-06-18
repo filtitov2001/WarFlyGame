f8893e1803a21662799c41facef84828df61335f
Felix Titov <titow.feliks@yandex.ru>
2022-06-18 18:11:27 +0300

    Created enemy movement

--- WarFlyGame/Enemy.swift
+++ WarFlyGame/Enemy.swift
@@ -24,6 +24,29 @@ class Enemy: SKSpriteNode {
         
     }
     
+    func flySpiral() {
+        let screenSize = UIScreen.main.bounds
+        let timeHorizontal: TimeInterval = 3
+        let timeVertical: TimeInterval = 10
+        
+        let moveLeft = SKAction.moveTo(x: 50, duration: timeHorizontal)
+        let moveRight = SKAction.moveTo(x: screenSize.width - 50, duration: timeHorizontal)
+        
+        moveLeft.timingMode = .easeInEaseOut
+        moveRight.timingMode = .easeInEaseOut
+        
+        let asideMovementSequence = SKAction.sequence([moveLeft, moveRight])
+        
+        let foreverAsideMovement = SKAction.repeatForever(asideMovementSequence)
+        
+        let forwardMovement = SKAction.moveTo(y: -105, duration: timeVertical)
+        
+        let groupMovement = SKAction.group([foreverAsideMovement, forwardMovement])
+        
+        self.run(groupMovement)
+        
+    }
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
