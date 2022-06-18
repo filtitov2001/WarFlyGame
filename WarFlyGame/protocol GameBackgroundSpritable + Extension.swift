da795f986e26aae100935009fd08c45c39fd3595
Felix Titov <titow.feliks@yandex.ru>
2022-06-18 13:09:58 +0300

    Added protocol and extension into new file

---
+++ WarFlyGame/protocol GameBackgroundSpritable + Extension.swift
@@ -0,0 +1,32 @@
+//
+//  protocol GameBackgroundSpritable + Extension.swift
+//  WarFlyGame
+//
+//  Created by Felix Titov on 6/18/22.
+//  Copyright © 2022 by Felix Titov. All rights reserved.
+//  
+
+
+import SpriteKit
+import GameplayKit
+
+protocol GameBackgroudSpritable {
+    static func populate() -> Self
+    static func randomPoint() -> CGPoint
+}
+
+extension GameBackgroudSpritable {
+    static func randomPoint() -> CGPoint {
+        let screen = UIScreen.main.bounds
+        let distribution = GKRandomDistribution(
+            lowestValue: Int(screen.size.height) + 100,
+            highestValue: Int(screen.size.height) + 200
+        )
+        
+        let y = CGFloat(distribution.nextInt())
+        let x = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.width)))
+                        
+        return CGPoint(x: x, y: y)
+                        
+    }
+}
